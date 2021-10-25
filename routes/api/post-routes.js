0bc328e5a905a9945ef3f01d9fd6d576e95c688a
Ting's Mac <huxu@uwplatt.edu>
2021-10-24 22:13:42 -0500

    Completed the section 3

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -47,6 +47,7 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
+  console.log(req.body);
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
