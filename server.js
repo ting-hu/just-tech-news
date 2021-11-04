b9dc7fc49edb3940d7afe5ff8f0131fdd07ccf0c
Ting's Mac <huxu@uwplatt.edu>
2021-11-04 01:40:52 -0500

    Stuck with 14.2.5

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
